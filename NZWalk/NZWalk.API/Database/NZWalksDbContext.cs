aee01e34bc6496383203d58078a2d892e412f58b
Shivam Gupta <shivamsinghal357@gmail.com>
2023-01-06 08:40:37 +0530

    commit: models+Database config+Migrations

---
+++ NZWalk/NZWalk.API/Database/NZWalksDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalk.API.Models.Domain;
+
+namespace NZWalk.API.Database
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options) 
+        {
+            //ctro of derived class is calling ctor of base class
+            //ctor of base class is a parametrised ctor which is taking params as Dbcontextoptions<type derived class>
+            //that's why we created a parametrized ctor of derived class
+        }
+
+        //create Db properties based on Models we created
+        //Entity framework take care of it
+        //Entity framework will create these tables on first time running (if tables doesn't exists)
+        //however we have made our first migration through nuget package manager console
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+    }
+}
---
+++ NZWalk/NZWalk.API/Models/Domain/Region.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalk.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }  //Guid: ised for hexadecimal digit sequence format
+        public string Name { get; set; }    
+        public string Code { get; set; }
+        public double Area { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public long Population { get; set; }
+
+        public IEnumerable<Walk> Walks { get; set; }
+
+    }
+}
